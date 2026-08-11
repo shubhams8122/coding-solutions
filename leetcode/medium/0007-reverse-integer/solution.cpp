090cd54278d6c15dc997524c11ad4b56c919f111
Shubham Singh <shubhamsingh92275@gmail.com>
2026-08-12 00:13:15 +0530

    ✅ Reverse Integer | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 8.5 MB | 2026-08-11

--- leetcode/medium/0007-reverse-integer/solution.cpp
+++ leetcode/medium/0007-reverse-integer/solution.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int reverse(int x) {
         int last_digit;
-        long n = 0;
+        int n = 0;
         while(x!=0){
             last_digit = x % 10;
+
+            if(n > INT_MAX /10 || (n == INT_MAX / 10 && last_digit > 7)) return 0;
+            if (n < INT_MIN / 10 || (n == INT_MIN / 10 && last_digit < -8)) return 0;
             n = n*10 + last_digit;
             x = x /10;
         }
