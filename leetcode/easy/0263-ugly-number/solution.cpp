1e2c468a0b518b545a7237f4234c8be3a19e9604
Shubham Singh <shubhamsingh92275@gmail.com>
2026-08-14 00:55:49 +0530

    ✅ Ugly Number | Easy | C++ | Runtime: 2 ms (6.18%) | Memory: 8.1 MB | 2026-08-13

---
+++ leetcode/easy/0263-ugly-number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        vector<int> factors ={2,3,5};
+        for(int i : factors){
+            while(n%i == 0){
+                n /= i;
+            }
+        }
+        return n == 1;
+    }
+};
