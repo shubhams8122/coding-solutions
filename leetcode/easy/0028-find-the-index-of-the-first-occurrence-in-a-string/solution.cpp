cd64e1bad27e2ebd20473a7fdd2d19a52fa3f5e0
Shubham Singh <shubhamsingh92275@gmail.com>
2026-09-04 23:32:49 +0530

    ✅ Find the Index of the First Occurrence in a String | Easy | C++ | Runtime: 23 ms (13.47%) | Memory: 72.8 MB | 2026-09-04

---
+++ leetcode/easy/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        if(m > n) return -1;
+        for(int i = 0; i <= n; i++){
+            if(haystack.substr(i,m)==needle){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
